4d77e8009d818508bbd05ce6b40f51490d550ae8
wyz <296022383@qq.com>
2021-08-03 01:18:46 +0800

    提交成功

---
+++ src/main/java/Demo/Git.java
@@ -0,0 +1,11 @@
+package Demo;
+
+/**
+ * @author wyz
+ * @create 2021-08-03 1:17
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("你好！");
+    }
+}
